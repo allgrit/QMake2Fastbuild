575d20a8ac4d8e7772d492af1b089c6506cefa79
allgrit <allgrit@forecastnow.ru>
2020-10-30 18:55:33 +0300

    test project and configuration of FastBuild scripts

---
+++ MyProject/generateInputFiles4Fbuild.bat
@@ -0,0 +1,35 @@
+@echo   Extract SOURCES from MyProject.pro...
+@echo .Sources = { > fbuild\files.bff
+@sed "1,/SOURCES\ +=\ \\/d" MyProject.pro | sed "/^$/,$d" | sed "s/^\s*\(.*\)\ \\/             '\1',/" | sed "s/^\s*\(.*\)\\/             '\1',/" | sed "s/^\s*\(.*[^,]\)$/             '\1'/" >> fbuild\files.bff
+@echo            } >> fbuild\files.bff
+
+@echo   Extract HEADERS from MyProject.pro...
+@echo .Headers = { >> fbuild\files.bff
+@sed "1,/HEADERS\ +=\ \\/d" MyProject.pro | sed "/^$/,$d" | sed "s/^\s*\(.*\)\ \\/             '\1',/" | sed "s/^\s*\(.*\)\\/             '\1',/" | sed "s/^\s*\(.*[^,]\)$/             '\1'/" >> fbuild\files.bff
+@echo            } >> fbuild\files.bff
+
+@echo   Extract FORMS from MyProject.pro...
+@echo .Forms   = { >> fbuild\files.bff
+@sed "1,/FORMS\ +=\ \\/d" MyProject.pro | sed "/^$/,$d" | sed "s/^\s*\(.*\)\ \\/             '\1',/" | sed "s/^\s*\(.*\)\\/             '\1',/" | sed "s/^\s*\(.*[^,]\)$/             '\1'/" >> fbuild\files.bff
+@echo            } >> fbuild\files.bff
+
+@echo   Extract RESOURCES from MyProject.pro...
+@echo .Resources={ >> fbuild\files.bff
+@sed "1,/RESOURCES\ +=\ \\/d" MyProject.pro | sed "/^$/,$d" | sed "s/^\s*\(.*\)\ \\/             '\1',/" | sed "s/^\s*\(.*\)\\/             '\1',/" | sed "s/^\s*\(.*[^,]\)$/             '\1'/" >> fbuild\files.bff
+@echo            } >> fbuild\files.bff
+
+@cd fbuild
+@mkdir tmp_qmake
+@cd tmp_qmake
+@echo   Creating dummy project...
+@call "C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\vc\Auxiliary\Build\vcvars64.bat"
+@C:\Qt\Qt5.12.6\5.12.6\msvc2017_64\bin\qmake.exe ..\..\MyProject.pro
+@echo   Find MOCables...
+@echo .Mocables= { >> ..\files.bff
+@grep "moc.exe $(DEFINES)" Makefile.Release | sed "s/^.*\ \.\.[\\\/]\.\.[\\\/]\(.*\)\ -o\ .*moc_.*cpp$/\1/" | sed "s/\\/\//g" | sed "s/^/             '/" | sed "s/$/',/" | sed "$,$s/,//" >> ..\files.bff
+@echo            } >> ..\files.bff
+@echo   Cleaning up...
+@cd ..
+@rmdir /S /Q tmp_qmake
+@cd ..
+
